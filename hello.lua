dcdd98b2f89bd86ff08f0c697cb5b35ace36b35d
Ketho <sghs17@outlook.com>
2025-03-05 09:40:02 +0100

    colors

--- hello.lua
+++ hello.lua
@@ -1,1 +1,7 @@
 print("hello from", _VERSION)
+
+print("\27[31mThis is red text\27[0m")
+print("\27[32mThis is green text\27[0m")
+print("\27[33mThis is yellow text\27[0m")
+print("\27[34mThis is blue text\27[0m")
+print("\27[1;35mThis is bold magenta text\27[0m")  -- Bold magenta
--- pwntools.py
+++ pwntools.py
@@ -4,3 +4,12 @@ print(asm('nop'))
 print(asm('nop', arch='arm'))
 
 print("hello pwn")
+
+log.success("This is a success message!")  # Green
+log.info("This is an info message.")       # Blue
+log.warn("This is a warning message!")     # Yellow
+log.failure("This is a failure message.")  # Red
+
+print(term.text.bold_red("This is bold and red!"))
+print(term.text.underline("This is underlined!"))
+print(term.text.bold_green("Bold green message"))
